6a9774b501d6b21d6dd0de03c8a09724d3f8393d
dsgnrr <dsgnrr@users.noreply.github.com>
2023-04-18 22:42:33 +0300

    TypeScript introduction

--- public/JS/1.js
+++ public/JS/1.js
@@ -1,2 +1,8 @@
 "use strict";
-var a = 10;
+let a = 10;
+a = true;
+let arr1 = [];
+let arr2 = [];
+let arr3 = ['2', 1, 2];
+const div = document.getElementById("div1");
+div.textContent = "as";
--- public/TS/1.ts
+++ public/TS/1.ts
@@ -1,2 +1,20 @@
-let a = 10;
+let a: number | boolean = 10;
+a=true;
+
+let arr1:(number|string)[]=[];
+let arr2:Array<number|string>=[];
+
+let arr3:[string,number,number]=['2',1,2];
+
+type Cat={
+    breed:string;
+    age:number;
+    move:()=>void;
+}
+
+const div = document.getElementById("div1")!;
+div.textContent="as";
+
+
+
 
