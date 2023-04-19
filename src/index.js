3cbc685c51abd9fae7da72f1701edd2856920972
dsgnrr <dsgnrr@users.noreply.github.com>
2023-04-19 18:33:07 +0300

    dz complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import { printSmth } from "./2";
 import { sayHello } from "./1";
-
+import './style.css';
 printSmth('Magic');
 sayHello();
+
+function component() {
+    const element = document.createElement('div');
+ 
+    // Lodash, now imported by this script
+    element.innerHTML = "<p>Created in js</p>";
+ 
+   element.classList.add('hello');
+ 
+    return element;
+  }
+ 
+  document.body.appendChild(component());
